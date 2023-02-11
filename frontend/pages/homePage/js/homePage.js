af565b5e56282c457b0695a9826c2c40fd76e7a7
dzennank <dzenankrlic@gmail.com>
2023-02-11 14:48:14 +0100

    displaying clothes on homepage

--- frontend/pages/homePage/js/homePage.js
+++ frontend/pages/homePage/js/homePage.js
@@ -1,20 +1,53 @@
 import { fatchHomePageData } from 'http://localhost/Web%20Programiranje/frontend/pages/homePage/js/fatchHomePageData.js';
 
-fatchHomePageData().then(([supp, equip, clothes]) => {
+const clothesContainer = document.getElementById("products-cont")
+console.log(clothesContainer)
+
+fatchHomePageData().then(([supp, clothes, equip]) => {
   console.log("SUPLEMENTI: ", supp)
   console.log("OPREMA: ", equip)
   console.log("ODECA: ", clothes)
+
+clothes.forEach(clot => {
+  const clotBox = document.createElement('div');
+  clotBox.classList.add('product');
+
+  const hover = document.createElement('div')
+  hover.classList.add('product-hover');
+
+  const clotImg = document.createElement('img');
+  clotImg.src = clot.clothes_image
+  clotImg.alt = clot.clothes_name
+
+  const clotName = document.createElement('h3')
+  clotName.textContent = clot.clothes_name;
+
+  const clotPrice = document.createElement('p');
+  clotPrice.textContent = clot.clothes_price;
+
+  const viewMoreButton = document.createElement('a');
+  viewMoreButton.textContent = 'View More';
+
+  clotBox.appendChild(clotImg);
+  clotBox.appendChild(clotName);
+  hover.appendChild(clotPrice);
+  hover.appendChild(viewMoreButton)
+
+  clotBox.appendChild(hover)
+  clothesContainer.appendChild(clotBox);
+
+}); 
 });
 
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n) {
+ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-
+window.plusSlides = plusSlides;
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slider-container")[0].children;
