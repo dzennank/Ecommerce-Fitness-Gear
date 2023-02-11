5f75c79858b869a06a96d7ce7a776dff78685328
dzennank <dzenankrlic@gmail.com>
2023-02-11 13:07:07 +0100

    added clothes and equipment conrollers

---
+++ backend/api/equipment_controller.php
@@ -0,0 +1,19 @@
+<?php
+include("../dbcon.php");
+
+$query = "SELECT * FROM equipment";
+$query_run = mysqli_query($conn, $query);
+
+
+if (mysqli_num_rows($query_run) > 0) {
+    
+    $products = array();
+
+    // Fetch each row and store it in the array
+    while($row = mysqli_fetch_assoc($query_run)) {
+        $products[] = $row;
+    }
+    echo json_encode($products);
+  } else {
+    echo json_encode(array("message" => "No products found."));
+  }
