924044e603725a87126c3f2a2c3eaaca041da174
dzennank <dzenankrlic@gmail.com>
2023-02-14 16:44:41 +0100

    creating new product page

--- frontend/pages/createNewProductPage/newProduct.js
+++ frontend/pages/createNewProductPage/newProduct.js
@@ -4,6 +4,8 @@ const handleOnChange = () => {
 
     console.log(selectedTypeValue)
     const form = document.getElementById("product-form");
+    const container = document.getElementById("container");
+    container.innerHTML = "";
     console.log(form)
     if(selectedTypeValue === "clothes") {
         
@@ -16,16 +18,18 @@ const handleOnChange = () => {
         male.textContent = "musko"
         female.textContent = "zensko"
 
-        form.appendChild(genderLabel);
-        form.appendChild(gender);
+        container.appendChild(genderLabel);
+        container.appendChild(gender);
         gender.appendChild(male);
         gender.appendChild(female);
 
         //creating size select
         const sizeLabel = document.createElement('label');
         sizeLabel.textContent = "Size"
+        sizeLabel.id = "size-label"
 
         const size = document.createElement('select');
+        size.id = "clothes-size"
         const s = document.createElement('option');
         const m = document.createElement('option');
         const l = document.createElement('option');
@@ -37,8 +41,8 @@ const handleOnChange = () => {
         xl.textContent = "XL"
         xxl.textContent = "XXL"
 
-        form.appendChild(sizeLabel);
-        form.appendChild(size);
+        container.appendChild(sizeLabel);
+        container.appendChild(size);
 
         size.appendChild(s);
         size.appendChild(m);
@@ -49,6 +53,77 @@ const handleOnChange = () => {
         const button = document.createElement('button');
         button.type = "submit"
         button.textContent = "Create"
-        form.appendChild(button);
+        container.appendChild(button);
+    } 
+
+    if(selectedTypeValue === "supplements") {
+        const itemToRemove = document.getElementById("size-label");
+        if(itemToRemove) {
+
+            itemToRemove.remove();
+        }
+        //creating gender select
+        const typeLabel = document.createElement('label');
+        typeLabel.textContent = "Type Of Supplement";
+        const type = document.createElement('select');
+        const protein = document.createElement('option');
+        const creatine = document.createElement('option')
+        selectTypeOption = document.createElement('option');
+        selectTypeOption.textContent = "Select Type Of Supplement"
+        selectTypeOption.hidden = true;
+        protein.textContent = "protein"
+        creatine.textContent = "kreatin"
+
+        container.appendChild(typeLabel);
+        container.appendChild(type);
+        type.appendChild(selectTypeOption);
+        type.appendChild(protein);
+        type.appendChild(creatine);
+
+        //creating size select
+        const weightLabel = document.createElement('label');
+        weightLabel.textContent = "Weight"
+
+        const size = document.createElement('select');
+        const s = document.createElement('option');
+        const m = document.createElement('option');
+        const l = document.createElement('option');
+        const xl = document.createElement('option');
+        const xxl = document.createElement('option');
+        s.textContent = "200gr"
+        m.textContent = "300gr"
+        l.textContent = "2kg"
+        xl.textContent = "3kg"
+        xxl.textContent = "5kg"
+
+        container.appendChild(weightLabel);
+        container.appendChild(size);
+
+        size.appendChild(s);
+        size.appendChild(m);
+        size.appendChild(l);
+        size.appendChild(xl);
+        size.appendChild(xxl);
+
+        const button = document.createElement('button');
+        button.type = "submit"
+        button.textContent = "Create"
+        container.appendChild(button);
+    }
+
+    if(selectedTypeValue === "equipment") {
+        const typeLabel = document.createElement('label');
+        typeLabel.textContent = "Type Of Equipment";
+
+        const input = document.createElement('input')
+        input.type = "text";
+
+        container.appendChild(typeLabel);
+        container.appendChild(input);
+
+        const button = document.createElement('button');
+        button.type = "submit"
+        button.textContent = "Create"
+        container.appendChild(button);
     }
 }
