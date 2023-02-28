ccb5b2d08dedcbc6d33ce0acb9c0626b9336e4c7
dzennank <dzenankrlic@gmail.com>
2023-02-28 19:26:04 +0100

    created tokenAuth controller

---
+++ backend/api/tokenAuth_controller.php
@@ -0,0 +1,23 @@
+<?php 
+
+class tokenAuthController {
+    public function tokenAuth() {
+
+        include("../dbcon.php");
+        $token = $_POST['token'];
+        
+        $query = "SELECT * FROM users WHERE user_id = (select user_id from tokens where token = '$token')";
+        $query_run = mysqli_query($conn, $query);
+        $user_arr = mysqli_fetch_array($query_run);
+        if(mysqli_num_rows($query_run) > 0) {
+            $response = array("firstName" => $user_arr['ime'], "lastName" => $user_arr['prezime'], "role" => $user_arr['tip_korisnika'], 'email' => $user_arr['email']);
+        } else {
+            http_response_code(401);
+            $response = array('error' => 'Invalid credentials');
+        }
+        echo json_encode($response);
+    }
+}
+
+$tokenAuth_controller = new tokenAuthController();
+$tokenAuth_controller -> tokenAuth();
