d0a36b029eb80fa789d4766fc09445203803d4aa
dzennank <dzenankrlic@gmail.com>
2023-02-28 20:15:23 +0100

    updated tokenAuth controller

--- backend/api/tokenAuth_controller.php
+++ backend/api/tokenAuth_controller.php
@@ -1,15 +1,23 @@
-<?php 
+<?php
 
-class tokenAuthController {
-    public function tokenAuth() {
+class tokenAuthController
+{
+    public function tokenAuth()
+    {
 
         include("../dbcon.php");
-        $token = $_POST['token'];
-        
+        $headers = apache_request_headers();
+        if (!isset($headers['Authorization'])) {
+            http_response_code(401);
+            exit();
+        }
+        $auth_header = $headers['Authorization'];
+        $token = str_replace('Bearer ', '', $auth_header);
+
         $query = "SELECT * FROM users WHERE user_id = (select user_id from tokens where token = '$token')";
         $query_run = mysqli_query($conn, $query);
         $user_arr = mysqli_fetch_array($query_run);
-        if(mysqli_num_rows($query_run) > 0) {
+        if (mysqli_num_rows($query_run) > 0) {
             $response = array("firstName" => $user_arr['ime'], "lastName" => $user_arr['prezime'], "role" => $user_arr['tip_korisnika'], 'email' => $user_arr['email']);
         } else {
             http_response_code(401);
@@ -20,4 +28,4 @@ class tokenAuthController {
 }
 
 $tokenAuth_controller = new tokenAuthController();
-$tokenAuth_controller -> tokenAuth();
+$tokenAuth_controller->tokenAuth();
