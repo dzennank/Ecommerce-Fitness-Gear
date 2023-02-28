797d466e0252f20cc75794c0752d1844687920f3
dzennank <dzenankrlic@gmail.com>
2023-02-28 19:10:59 +0100

    created token for auth

--- backend/api/login_controller.php
+++ backend/api/login_controller.php
@@ -10,7 +10,12 @@ $query_run = mysqli_query($conn, $query);
 $user_arr = mysqli_fetch_array($query_run);
 
 if (mysqli_num_rows($query_run) > 0) {
-  $response = array("success" => true, "ime" => $user_arr['ime'], "prezime" => $user_arr['prezime'], "tip" => $user_arr['tip_korisnika'], 'email' => $user_arr['email']);
+  $user_id = $user_arr['user_id'];
+  $token = bin2hex(random_bytes(16));
+  $query_token = "INSERT INTO tokens (user_id, token) values ('$user_id', '$token')";
+  $query_token_run = mysqli_query($conn, $query_token);
+  // $response = array("success" => true, "ime" => $user_arr['ime'], "prezime" => $user_arr['prezime'], "tip" => $user_arr['tip_korisnika'], 'email' => $user_arr['email'], 'token' => $token);
+  $response = array("success" => true,'token' => $token);
 } else {
   $response = array("success" => false);
 }
