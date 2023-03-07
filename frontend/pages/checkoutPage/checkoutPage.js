759b93147c70429650f8f87b4a5153484c9916ff
dzennank <dzenankrlic@gmail.com>
2023-03-07 15:29:13 +0100

    fixed checkout

--- frontend/pages/checkoutPage/checkoutPage.js
+++ frontend/pages/checkoutPage/checkoutPage.js
@@ -1,15 +1,16 @@
-import { checkUser } from "../../getUser.js";
+import { getUser } from "../../getUser.js";
 
 
-checkUser()
+
 
 const handleOrderButton = () => {
     const cartData = JSON.parse(localStorage.getItem("productForCart"));
     console.log(cartData)
-    const user = JSON.parse(localStorage.getItem("user"));
-    // const emailInput = document.getElementById("email");
-    const email = user.email;
-    fetch("../../../backend/api/orders_controller.php", {
+    getUser().then(user => {
+      console.log(user)
+      const email = user.email;
+
+      fetch("../../../backend/api/orders_controller.php", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded"
@@ -35,4 +36,50 @@ const handleOrderButton = () => {
                 console.log("OVO NE RADI NESTO", data)
             }
         })
+    })
+    // const user = JSON.parse(localStorage.getItem("user"));
+    // const emailInput = document.getElementById("email");
+    
+   
 }
+
+const button = document.getElementById("checkout-submit");
+button.addEventListener("click", function(){
+
+  const cartData = JSON.parse(localStorage.getItem("productForCart"));
+    console.log(cartData)
+    getUser().then(user => {
+      console.log(user)
+      const email = user.email;
+
+      fetch("../../../backend/api/orders_controller.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `email=${email}`
+      })
+        .then(response => response.json())
+        .then(data => { 
+            console.log(data)
+            if(data.success) {
+                fetch("../../../backend/api/orderItems_controller.php", {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: JSON.stringify(cartData)
+                  })
+                  .then(response => response.json())
+                  .then(data => {
+                    console.log(data)
+                  })
+            } else {
+                console.log("OVO NE RADI NESTO", data)
+            }
+        })
+    })
+    // const user = JSON.parse(localStorage.getItem("user"));
+    // const emailInput = document.getElementById("email");
+
+});
