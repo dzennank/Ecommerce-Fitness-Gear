669facad44000fc29a4c145eaeb7f4672700708d
dzennank <dzenankrlic@gmail.com>
2023-03-13 16:18:05 +0100

    changes in send mial controller

--- backend/api/sendMail_controller.php
+++ backend/api/sendMail_controller.php
@@ -3,12 +3,13 @@
 class SendMailController {
     
     public function SendMail() {
+      
        require "../email/emailConfig.php";
 
         $email = $_POST["email"];
         $username = $_POST["username"];
 
-        $response = sendMail($email, $username) ? array("success" => true) : array("success" => false);
+       sendMail($email, $username);
 
     
         
