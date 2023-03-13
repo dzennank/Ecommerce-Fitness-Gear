73276d916d217946f62db30648f235f515e0139b
dzennank <dzenankrlic@gmail.com>
2023-03-13 16:18:57 +0100

    createt email template

---
+++ backend/email/emailTemplate.php
@@ -0,0 +1,81 @@
+<?php 
+function generete_email_template() {
+  include("../dbcon.php");
+    $sql = "SELECT oi.product_type, oi.quantity, 
+    CASE 
+    WHEN oi.product_type = 'supplement' THEN s.supplement_name 
+    WHEN oi.product_type = 'clothes' THEN c.clothes_name 
+    END AS product_name
+    FROM order_items oi
+    LEFT JOIN supplements s ON oi.product_id = s.supplement_id AND oi.product_type = 'supplement'
+    LEFT JOIN clothes c ON oi.product_id = c.clothes_id AND oi.product_type = 'clothes'
+    WHERE oi.order_id = (SELECT MAX(order_id) FROM orders)";
+    
+    // execute query
+    $result = mysqli_query($conn, $sql);
+    // fetch data into array
+    $data = array();
+    if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+    
+    }
+    
+    }
+  
+    $tableRows = '';
+    foreach ($data as $d) {
+      $tableRows .= '<tr>
+                      <td style="padding: 10px; text-align: left; border: 1px solid #ccc;">' . $d['product_name'] . '</td>
+                      <td style="padding: 10px; text-align: left; border: 1px solid #ccc;">' . $d['quantity'] . '</td>
+                      <td style="padding: 10px; text-align: left; border: 1px solid #ccc;">Price</td>
+                    </tr>';
+    }
+  
+    // Replace the placeholders in the HTML code with the actual values
+    return '<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Confirmation of Purchase</title>
+      </head>
+      <body style="font-family: Arial, sans-serif; font-size: 16px; line-height: 1.5; background-color: #f5f5f5; margin: 0; padding: 0;">
+        <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); overflow: hidden;">
+          <div style="background-color: #eaeaea; padding: 20px; text-align: center;">
+            <h1 style="margin: 0; font-size: 24px; color: #333;">Confirmation of Purchase</h1>
+          </div>
+          <div style="padding: 20px;">
+            <p style="margin: 0 0 20px 0;">Dear [Customer Name],</p>
+            <p style="margin: 0 0 20px 0;">Thank you for your recent purchase on [E-commerce App Name]. We are pleased to confirm your order details below:</p>
+            <table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
+              <thead>
+                <tr style="background-color: #eaeaea;">
+                  <th style="padding: 10px; text-align: left; border: 1px solid #ccc;">Product</th>
+                  <th style="padding: 10px; text-align: left; border: 1px solid #ccc;">Quantity</th>
+                  <th style="padding: 10px; text-align: left; border: 1px solid #ccc;">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                ' . $tableRows . '
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td style="padding: 10px; text-align: left; border: 1px solid #ccc;">Total</td>
+                  <td style="padding: 10px; text-align: left; border: 1px solid #ccc;"></td>
+                  <td style="padding: 10px; text-align: left; border: 1px solid #ccc;">[Total Price]</td>
+                </tr>
+                </tfoot>
+        </table>
+        <p style="margin: 0 0 20px 0;">Your order will be shipped to:</p>
+        <p style="margin: 0 0 20px 0;">[Customer Address]</p>
+        <p style="margin: 0 0 20px 0;">Thank you for shopping with us!</p>
+      </div>';
+
+}
+
+// generete_email_template();
+
+
+
+
