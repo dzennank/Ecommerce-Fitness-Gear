42efbf2aeaa80da6aa957f430ba8307d7719fd7a
dzennank <dzenankrlic@gmail.com>
2023-03-21 16:23:28 +0100

    deleting product frontend part

--- frontend/pages/singleProductPage/singleProduct.js
+++ frontend/pages/singleProductPage/singleProduct.js
@@ -1,13 +1,42 @@
 import { checkUser } from "../../getUser.js";
+import { getUser } from "../../getUser.js";
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const name = urlParams.get("name");
+checkUser();
+getUser().then((user) => {
+  console.log(user);
+  if (user.role === "Admin") {
+    const infoCont = document.getElementById("info-container");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.id = "delete-btn";
+    deleteBtn.innerText = "Delete Product";
+    infoCont.appendChild(deleteBtn);
 
-checkUser()
+    document.getElementById("delete-btn").addEventListener("click", () => {
+      const product = JSON.parse(localStorage.getItem("productData"));
+      console.log(product);
+      const productType = product.name;
+
+      fetch("../../../backend/api/deleteProduct_controller.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `name=${name}&id=${id}`,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          alert("Product succesfully deleted");
+          location.href = "../homePage/homePage.html";
+        });
+    });
+  }
+});
 
 const fatchSingleProductData = () => {
   return new Promise(function (resolve, reject) {
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get("id");
-    const name = urlParams.get("name");
-
     const xhr = new XMLHttpRequest();
     xhr.open(
       "GET",
@@ -30,10 +59,10 @@ const fatchSingleProductData = () => {
 
 fatchSingleProductData()
   .then((response) => {
-    // console.log(response)
+    console.log(response);
     // console.log(response.data);
     localStorage.setItem("productData", JSON.stringify(response));
-    
+
     if (response.name === "clothes") {
       const productName = document.getElementById("productName");
       const price = document.getElementById("price");
@@ -67,53 +96,36 @@ fatchSingleProductData()
     console.log(err);
   });
 
-  
-let productToAdd = []
-const button = document.getElementById('addToCart');
-console.log("adadsa",button)
-button.addEventListener("click", function() {
+let productToAdd = [];
+const button = document.getElementById("addToCart");
+console.log("adadsa", button);
+button.addEventListener("click", function () {
   console.log("first");
-  const confirmationMessage = document.getElementById('confirmation-message');
-    let cartData = JSON.parse(localStorage.getItem('productForCart')) || [];
+  const confirmationMessage = document.getElementById("confirmation-message");
+  let cartData = JSON.parse(localStorage.getItem("productForCart")) || [];
 
-    const productData = JSON.parse(localStorage.getItem("productData"));
+  const productData = JSON.parse(localStorage.getItem("productData"));
 
-    const elIndex = cartData.findIndex(cI => cI.data[`${productData.name}_id`] === productData.data[`${productData.name}_id`])
+  const elIndex = cartData.findIndex(
+    (cI) =>
+      cI.data[`${productData.name}_id`] ===
+      productData.data[`${productData.name}_id`]
+  );
 
-    if (elIndex !== -1){
-      console.log("nadjeni index: ", elIndex)
-      cartData[elIndex].data.quantity += 1;
-    }
-    else{
-      productData.data.quantity = 1;
-      cartData.push(productData)
-    }
-    
-    localStorage.setItem("productData", JSON.stringify(productData));
-    localStorage.setItem("productForCart", JSON.stringify(cartData));
-    
-     // Show confirmation message
-     confirmationMessage.classList.add('show');
-     setTimeout(function() {
-      confirmationMessage.classList.remove('show');
-    }, 2000);
-})
+  if (elIndex !== -1) {
+    console.log("nadjeni index: ", elIndex);
+    cartData[elIndex].data.quantity += 1;
+  } else {
+    productData.data.quantity = 1;
+    cartData.push(productData);
+  }
 
-  // const addToCart = () => {
-  //   const confirmationMessage = document.getElementById('confirmation-message');
-  //   let cartData = JSON.parse(localStorage.getItem('productForCart')) || [];
+  localStorage.setItem("productData", JSON.stringify(productData));
+  localStorage.setItem("productForCart", JSON.stringify(cartData));
 
-  //   const productData = JSON.parse(localStorage.getItem("productData"));
-  //   cartData.push(productData)
-  //   localStorage.setItem("productForCart", JSON.stringify(cartData));
-    
-  //    // Show confirmation message
-  //    confirmationMessage.classList.add('show');
-  //    setTimeout(function() {
-  //     confirmationMessage.classList.remove('show');
-  //   }, 2000);
-    
-  // }
-
-  const test = JSON.parse(localStorage.getItem("productForCart"));
-  console.log(test)
+  // Show confirmation message
+  confirmationMessage.classList.add("show");
+  setTimeout(function () {
+    confirmationMessage.classList.remove("show");
+  }, 2000);
+});
