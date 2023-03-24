66804b3dc07f129a6c13393f94dd7fd0997da9f4
dzennank <dzenankrlic@gmail.com>
2023-03-24 13:31:54 +0100

    inserting uploaded image in db

--- backend/api/register_controller.php
+++ backend/api/register_controller.php
@@ -15,8 +15,12 @@ $phone = $_POST["phone"];
 $img = "neki_url";
 $user_type = $_POST["type"];
 // Connect to database and insert new user
+$target_dir = "../uploads/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
 
-$query = "INSERT INTO users (
+  $url = "../uploads" . basename($_FILES["image"]["name"]);
+  $query = "INSERT INTO users (
     ime, 
     prezime,
     email, 
@@ -41,15 +45,48 @@ $query = "INSERT INTO users (
         '$birthday',
         '$jmbg',
         '$phone',
-        '$img',
+        '$url',
         '$user_type'
         )";
-$result = mysqli_query($conn, $query);
+      $result = mysqli_query($conn, $query);
+      if ($result) {
+        $response = array("success" => true);
+      } else {
+        $response = array("success" => false);
+      }
+      
+      echo json_encode($response);
 
-if ($result) {
-  $response = array("success" => true);
-} else {
-  $response = array("success" => false);
 }
 
-echo json_encode($response);
+// $query = "INSERT INTO users (
+//     ime, 
+//     prezime,
+//     email, 
+//     sifra, 
+//     potvrda_sifra, 
+//     mesto_rodjenja, 
+//     drzava_rodjenja,
+//     datum_rodjenja,
+//     jmbg,
+//     telefon,
+//     slika_url,
+//     tip_korisnika
+//     ) 
+//     VALUES (
+//         '$username', 
+//         '$lastName', 
+//         '$email', 
+//         '$password', 
+//         '$confirmPassword', 
+//         '$place_birth', 
+//         '$country',
+//         '$birthday',
+//         '$jmbg',
+//         '$phone',
+//         '$img',
+//         '$user_type'
+//         )";
+
+
+
